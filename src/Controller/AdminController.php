6c6bff5f93542f722de7fbe8d0701f5ac499cd05
jojo <jordy.fatigba@theopentrade.com>
2020-11-19 10:47:10 +0000

    Implémentation de l'édition d'un bien immobilier.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Immo;
+use App\Form\ImmoType;
 use App\Repository\ImmoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 // annotations
@@ -41,15 +43,26 @@ class AdminController extends AbstractController
     /**
      * Afficher la page pour modifier un bien immobilier.
      *
-     * @Route("/admin/immos/update/{id}", name="immos.admin.edit", methods={"GET"})
+     * @Route("/admin/immos/update/{id}", name="immos.admin.edit", methods={"GET", "POST"})
      *
      * @param Immo $immo
+     * @param Request $req
      * @return Response
      */
-    public function edit(Immo $immo) : Response
+    public function edit(Immo $immo, Request $req) : Response
     {
+        $form = $this->createForm(ImmoType::class, $immo) ;
+        $form->handleRequest($req) ;
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush() ;
+            return $this->redirectToRoute("immos.admin") ;
+        }
+
         return $this->render("admin/edit.html.twig", [
-            "immos" => $immo
+            "immos" => $immo,
+            "form" => $form->createView()
         ]) ;
     }
 }
